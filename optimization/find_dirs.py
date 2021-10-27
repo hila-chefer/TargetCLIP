32e2e2060419e918260b180bb9bd1d06db02703d
Hila Chefer <38704554+hila-chefer@users.noreply.github.com>
2021-10-27 11:09:09 +0300

    Update find_dirs.py

--- optimization/find_dirs.py
+++ optimization/find_dirs.py
@@ -11,19 +11,20 @@ from criteria.clip_loss import CLIPLoss
 from models.stylegan2.model import Generator
 import math
 
+
 def get_latent(g_ema):
     mean_latent = g_ema.module.mean_latent(4096)
     latent_code_init_not_trunc = torch.randn(1, 512).cuda()
     with torch.no_grad():
-        _, latent_code_init,_ = g_ema([latent_code_init_not_trunc], return_latents=True,
-                                    truncation=args.truncation, truncation_latent=mean_latent)
+        _, latent_code_init, _ = g_ema([latent_code_init_not_trunc], return_latents=True,
+                                       truncation=args.truncation, truncation_latent=mean_latent)
 
     direction = latent_code_init.detach().clone()
     direction.requires_grad = True
     return direction
 
 
-def load_model():
+def load_model(args):
     g_ema = Generator(args.stylegan_size, 512, 8)
     g_ema.load_state_dict(torch.load(args.ckpt)["g_ema"], strict=False)
     g_ema.eval()
@@ -41,159 +42,160 @@ def get_lr(t, initial_lr, rampdown=0.75, rampup=0.005):
 
 
 def main(args):
-        g_ema = load_model()
+    g_ema = load_model(args)
 
-        dir_name = args.dir_name
+    dir_name = args.dir_name
 
-        if not os.path.exists(dir_name):
-            os.mkdir(dir_name)
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
 
-        NUM_DIRECTIONS = args.num_directions
-        NUM_IMAGES = args.num_images
-        directions = [get_latent(g_ema) for _ in range(NUM_DIRECTIONS)]
+    NUM_DIRECTIONS = args.num_directions
+    NUM_IMAGES = args.num_images
+    directions = [get_latent(g_ema) for _ in range(NUM_DIRECTIONS)]
 
-        directions_cat = torch.cat(directions)
+    directions_cat = torch.cat(directions)
+    with torch.no_grad():
         dirs, _ = g_ema([directions_cat], input_is_latent=True, randomize_noise=False)
         for j, latent in enumerate(directions):
             torchvision.utils.save_image(dirs[j], f"{dir_name}/dir_{j}.png", normalize=True, range=(-1, 1))
 
-        latents = [None] * NUM_IMAGES
-        if args.generated_images:
-            for n in range(NUM_IMAGES):
-                with torch.no_grad():
-                    latents[n] = get_latent(g_ema)
-                    latents[n].requires_grad = False
-        else:
-            data = torch.load(args.data_path)
-            for n in range(NUM_IMAGES):
-                with torch.no_grad():
-                    latents[n] = data[n].unsqueeze(0).cuda()
-                    latents[n].requires_grad = False
-        latents = torch.cat(latents)
-
-        clip_loss = CLIPLoss(args)
-        clip_loss = torch.nn.DataParallel(clip_loss)
-        optimizer = optim.Adam(directions, lr=args.lr, weight_decay=args.weight_decay)
-
-        import torchvision.transforms as transforms
-        from PIL import Image
-        transform = transforms.Compose([
-            transforms.Resize((args.stylegan_size, args.stylegan_size)),
-            transforms.ToTensor(),
-            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
-
-        if args.target_path is not None:
-            tgt_lat = None
-            # target is image from folder
+    latents = [None] * NUM_IMAGES
+    if args.generated_images:
+        for n in range(NUM_IMAGES):
             with torch.no_grad():
-                img_target = Image.open(args.target_path)
-                crop_size = min(img_target.size[0], img_target.size[1])
-                crop = transforms.Compose([
-                    transforms.CenterCrop(crop_size)])
-                img_target = crop(img_target)
-                img_target = transform(img_target).unsqueeze(0).cuda()
-                torchvision.utils.save_image(img_target, f"{dir_name}/target.png",
-                                             normalize=True, range=(-1, 1))
+                latents[n] = get_latent(g_ema)
+                latents[n].requires_grad = False
+    else:
+        data = torch.load(args.data_path)
+        for n in range(NUM_IMAGES):
+            with torch.no_grad():
+                latents[n] = data[n].unsqueeze(0).cuda()
+                latents[n].requires_grad = False
+    latents = torch.cat(latents)
+
+    clip_loss = CLIPLoss(args)
+    clip_loss = torch.nn.DataParallel(clip_loss)
+    optimizer = optim.Adam(directions, lr=args.lr, weight_decay=args.weight_decay)
+
+    import torchvision.transforms as transforms
+    from PIL import Image
+    transform = transforms.Compose([
+        transforms.Resize((args.stylegan_size, args.stylegan_size)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+
+    if args.target_path is not None:
+        tgt_lat = None
+        # target is image from folder
+        with torch.no_grad():
+            img_target = Image.open(args.target_path)
+            crop_size = min(img_target.size[0], img_target.size[1])
+            crop = transforms.Compose([
+                transforms.CenterCrop(crop_size)])
+            img_target = crop(img_target)
+            img_target = transform(img_target).unsqueeze(0).cuda()
+            torchvision.utils.save_image(img_target, f"{dir_name}/target.png",
+                                         normalize=True, range=(-1, 1))
+            target_clip = clip_loss.module.encode(img_target)
+            target_clip = target_clip / target_clip.norm(dim=-1)
+
+    for dir_idx, direction in enumerate(directions):
+
+        coefficients = [None] * NUM_IMAGES
+        for n in range(NUM_IMAGES):
+            coefficient = torch.ones(1).to("cuda")
+            coefficient.requires_grad = True
+            coefficients[n] = coefficient
+
+        if args.target_path is None:
+            # target is latent dir
+            with torch.no_grad():
+                img_target, _ = g_ema([direction], input_is_latent=True, randomize_noise=False)
                 target_clip = clip_loss.module.encode(img_target)
                 target_clip = target_clip / target_clip.norm(dim=-1)
+                tgt_lat = torch.clone(direction)
+                tgt_lat = tgt_lat.detach()
 
-        for dir_idx, direction in enumerate(directions):
+        opt_loss = torch.Tensor([float("Inf")]).to("cuda")
+        pbar = tqdm(range(args.step))
 
-            coefficients = [None] * NUM_IMAGES
-            for n in range(NUM_IMAGES):
-                coefficient = torch.ones(1).to("cuda")
-                coefficient.requires_grad = True
-                coefficients[n] = coefficient
+        for i in pbar:
+            # calculate learning rate
+            t = i / args.step
+            lr = get_lr(t, args.lr)
+            optimizer.param_groups[0]["lr"] = lr
 
-            if args.target_path is None:
-                # target is latent dir
-                with torch.no_grad():
-                    img_target, _ = g_ema([direction], input_is_latent=True, randomize_noise=False)
-                    target_clip = clip_loss.module.encode(img_target)
-                    target_clip = target_clip / target_clip.norm(dim=-1)
-                    tgt_lat = torch.clone(direction)
-                    tgt_lat = tgt_lat.detach()
+            optimizer_coeffs = optim.Adam(coefficients, lr=args.lr, weight_decay=0.01)
+            loss = torch.zeros(1).cuda()
+            target_semantic = torch.zeros(1).cuda()
 
-            opt_loss = torch.Tensor([float("Inf")]).to("cuda")
-            pbar = tqdm(range(args.step))
+            with torch.no_grad():
+                img_gen, _ = g_ema([latents], input_is_latent=True, randomize_noise=False)
+                image_gen_clip = clip_loss.module.encode(img_gen)
 
-            for i in pbar:
-                # calculate learning rate
-                t = i / args.step
-                lr = get_lr(t, args.lr)
-                optimizer.param_groups[0]["lr"] = lr
+            direction_with_coeff = [direction * coefficients[i] for i in range(args.num_images)]
+            direction_with_coeff = torch.stack(direction_with_coeff).squeeze(1).cuda()
+            img_gen_amp, _ = g_ema([latents + direction_with_coeff], input_is_latent=True, randomize_noise=False)
 
-                optimizer_coeffs = optim.Adam(coefficients, lr=args.lr, weight_decay=0.01)
-                loss = torch.zeros(1).cuda()
-                target_semantic = torch.zeros(1).cuda()
+            image_gen_amp_clip = clip_loss.module.encode(img_gen_amp)
+            diffs = image_gen_amp_clip - image_gen_clip
+            diffs = diffs / diffs.norm(dim=-1, keepdim=True)
 
-                with torch.no_grad():
-                    img_gen, _ = g_ema([latents], input_is_latent=True, randomize_noise=False)
-                    image_gen_clip = clip_loss.module.encode(img_gen)
+            # transfer loss
+            image_gen_amp_clip_norm = image_gen_amp_clip / image_gen_amp_clip.norm(dim=-1, keepdim=True)
+            similarity_gap = image_gen_amp_clip_norm @ target_clip.T
+            target_semantic += 1 - similarity_gap.mean()
 
-                direction_with_coeff = [direction * coefficients[i] for i in range(args.num_images)]
-                direction_with_coeff = torch.stack(direction_with_coeff).squeeze(1).cuda()
-                img_gen_amp, _ = g_ema([latents + direction_with_coeff], input_is_latent=True, randomize_noise=False)
+            if args.lambda_consistency > 0:
+                diffs_mat_amp = diffs @ diffs.T
+                ones_mat = torch.ones(diffs_mat_amp.shape[0]).cuda()
+                similarities_loss = torch.sum(ones_mat - diffs_mat_amp) / (NUM_IMAGES ** 2 - NUM_IMAGES)
+                loss += args.lambda_consistency * similarities_loss
 
-                image_gen_amp_clip = clip_loss.module.encode(img_gen_amp)
-                diffs = image_gen_amp_clip - image_gen_clip
-                diffs = diffs / diffs.norm(dim=-1, keepdim=True)
+            # add semantic transfer loss
+            loss += args.lambda_transfer * target_semantic.reshape(loss.shape)
 
-                # transfer loss
-                image_gen_amp_clip_norm = image_gen_amp_clip / image_gen_amp_clip.norm(dim=-1, keepdim=True)
-                similarity_gap = image_gen_amp_clip_norm @ target_clip.T
-                target_semantic += 1 - similarity_gap.mean()
-				
-                if args.lambda_consistency > 0:
-                    diffs_mat_amp = diffs @ diffs.T
-                    ones_mat = torch.ones(diffs_mat_amp.shape[0]).cuda()
-                    similarities_loss = torch.sum(ones_mat - diffs_mat_amp) / (NUM_IMAGES ** 2 - NUM_IMAGES)
-                    loss += args.lambda_consistency * similarities_loss
-
-                # add semantic transfer loss
-                loss += args.lambda_transfer * target_semantic.reshape(loss.shape)
-
-                pbar.set_description(
-                    (
-                        f"loss: {loss.item():.4f}; consistency loss: {similarities_loss.view(-1).item():.4f};"
-                        f"transfer loss: {target_semantic.item():.4f}; "
-                        f"lr: {lr:.4f}; norm: {direction.norm().item():.4f}"
-                    )
+            pbar.set_description(
+                (
+                    f"loss: {loss.item():.4f}; consistency loss: {similarities_loss.view(-1).item():.4f};"
+                    f"transfer loss: {target_semantic.item():.4f}; "
+                    f"lr: {lr:.4f}; norm: {direction.norm().item():.4f}"
                 )
+            )
 
-                optimizer.zero_grad()
-                optimizer_coeffs.zero_grad()
-                loss.backward()
-                optimizer_coeffs.step()
-                optimizer.step()
+            optimizer.zero_grad()
+            optimizer_coeffs.zero_grad()
+            loss.backward()
+            optimizer_coeffs.step()
+            optimizer.step()
 
-                with torch.no_grad():
+            with torch.no_grad():
 
-                    if loss < opt_loss:
-                        numpy.save('{0}/direction{1}.npy'.format(args.dir_name, dir_idx),
-                                   direction.detach().cpu().numpy())
-                        opt_loss = loss
+                if loss < opt_loss:
+                    numpy.save('{0}/direction{1}.npy'.format(args.dir_name, dir_idx),
+                               direction.detach().cpu().numpy())
+                    opt_loss = loss
 
-                        if tgt_lat is not None:
-                            img_gen_tgt, _ = g_ema([tgt_lat + direction], input_is_latent=True, randomize_noise=False)
-                            torchvision.utils.save_image(img_gen_tgt, f"{dir_name}/img_gen_{dir_idx}_tgt.png",
-                                                         normalize=True, range=(-1, 1))
-                            img_gen_tgt, _ = g_ema([tgt_lat - direction], input_is_latent=True, randomize_noise=False)
-                            torchvision.utils.save_image(img_gen_tgt, f"{dir_name}/img_gen_{dir_idx}_tgt_red.png",
-                                                         normalize=True, range=(-1, 1))
+                    if tgt_lat is not None:
+                        img_gen_tgt, _ = g_ema([tgt_lat + direction], input_is_latent=True, randomize_noise=False)
+                        torchvision.utils.save_image(img_gen_tgt, f"{dir_name}/img_gen_{dir_idx}_tgt.png",
+                                                     normalize=True, range=(-1, 1))
+                        img_gen_tgt, _ = g_ema([tgt_lat - direction], input_is_latent=True, randomize_noise=False)
+                        torchvision.utils.save_image(img_gen_tgt, f"{dir_name}/img_gen_{dir_idx}_tgt_red.png",
+                                                     normalize=True, range=(-1, 1))
 
-                        img_gen, _ = g_ema([latents], input_is_latent=True, randomize_noise=False)
-                        direction_with_coeff = [direction * coefficients[i] for i in range(args.num_images)]
-                        direction_with_coeff = torch.stack(direction_with_coeff).squeeze(1).cuda()
-                        img_gen_amp, _ = g_ema([latents + direction_with_coeff], input_is_latent=True,
-                                               randomize_noise=False)
+                    img_gen, _ = g_ema([latents], input_is_latent=True, randomize_noise=False)
+                    direction_with_coeff = [direction * coefficients[i] for i in range(args.num_images)]
+                    direction_with_coeff = torch.stack(direction_with_coeff).squeeze(1).cuda()
+                    img_gen_amp, _ = g_ema([latents + direction_with_coeff], input_is_latent=True,
+                                           randomize_noise=False)
 
-                        for j in range(args.num_images):
-                            torchvision.utils.save_image(img_gen[j], f"{dir_name}/img_gen_{j}.png",
-                                                         normalize=True, range=(-1, 1))
-                            torchvision.utils.save_image(img_gen_amp[j],
-                                                         f"{dir_name}/img_gen_amp_{dir_idx}_{j}.png",
-                                                         normalize=True, range=(-1, 1))
+                    for j in range(args.num_images):
+                        torchvision.utils.save_image(img_gen[j], f"{dir_name}/img_gen_{j}.png",
+                                                     normalize=True, range=(-1, 1))
+                        torchvision.utils.save_image(img_gen_amp[j],
+                                                     f"{dir_name}/img_gen_amp_{dir_idx}_{j}.png",
+                                                     normalize=True, range=(-1, 1))
 
 
 if __name__ == "__main__":
